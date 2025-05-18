d797446180b0278bb3a2cf899ef4fd6b809e52b8
Vaman Prabhakar <vamanprabhakar03@gmail.com>
2025-05-18 23:15:02 +0530

    extracted and splited the pdf datas into chunks

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,31 @@
 import streamlit as st
-#Upload PDF files
-st.header("My first Chatbot")
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+#Step 1 : Uploading the file in the chat bot
+st.header("Welcome to Workspace")
 with st.sidebar:
     st.title("Your Documents")
     file = st.file_uploader(" Upload a PDf file and start asking questions",
 type="pdf")
+
+#Step 2 : Extract the text from the pdf
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text =""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+# splitting the datas from pdf into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators = "\n",
+        chunk_size = 300,
+        chunk_overlap = 50,
+        length_function= len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
+
+
+
+
